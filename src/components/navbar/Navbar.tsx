97d99c4ed429f9ac6d03a3d7265d5cdad1ae2eb0
alzC <69784004+alzC@users.noreply.github.com>
2025-03-16 00:58:55 +0100

    pre build

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,9 +4,14 @@ import styles from './Navbar.module.css';
 
 const Navbar = () => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
     };
 
     return (
@@ -19,17 +24,33 @@ const Navbar = () => {
                     <a href="#journal" onClick={toggleDropdown}>Journal Officiel</a>
                     {isDropdownOpen && (
                         <ul className={styles.dropdownMenu}>
-                            <li><a href="#nomination">Nomination</a></li>
+                            <li><a href="#nomination">Nominations</a></li>
                         </ul>
                     )}
                 </li>
             </ul>
-            <div className={styles.burger}>
+            <div className={styles.burger} onClick={toggleMenu}>
                 <div className={styles.line1}></div>
                 <div className={styles.line2}></div>
                 <div className={styles.line3}></div>
             </div>
-
+            {isMenuOpen && (
+                <div className={styles.fullscreenMenu}>
+                    <div className={styles.closeButton} onClick={toggleMenu}>×</div>
+                    <ul className={styles.menuLinks}>
+                        <li><a href="#home" onClick={toggleMenu}>Acceuil</a></li>
+                        <li><a href="#services" onClick={toggleMenu}>Archives</a></li>
+                        <li className={styles.dropdown}>
+                            <a href="#journal" onClick={toggleDropdown}>Journal Officiel</a>
+                            {isDropdownOpen && (
+                                <ul className={styles.dropdownMenu}>
+                                    <li><a href="#nomination" onClick={toggleMenu}>Nominations</a></li>
+                                </ul>
+                            )}
+                        </li>
+                    </ul>
+                </div>
+            )}
         </nav>
     );
 };
